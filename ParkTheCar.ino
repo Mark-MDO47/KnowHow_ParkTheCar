4040dc8a1b26da4ef13fa1cfebf0a8f2745ae996
Mark-MDO47 <Mark-MDO47@users.noreply.github.com>
2017-02-19 09:52:54 -0800

    vastly improved potentiometer hysterisis
    vastly improved potentiometer hysterisis; still need to fix interval selection

--- ParkTheCar.ino
+++ ParkTheCar.ino
@@ -13,8 +13,6 @@
 //
 // My buddy Jim and I pretty much re-wrote the code for our own sensibilities
 //
-// Using a moving average of the utrasound ranges so thats crazy individual readings don't goof us
-//
 // Discovered it REALLY helps to use AREF when using an analog signal; maybe that is needed due to
 //    using inexpensive clone Arduino nano
 // Also using a software hysteresis. When the potentiometer is found to be in a certain interval
@@ -36,6 +34,11 @@
 // uses Uxcell 50K-ohm Trimmer Potentiometer. I used the 3386P-package top-adjustment version
 //   example: https://smile.amazon.com/gp/product/B00G9JSCUQ/ref=oh_aui_detailpage_o02_s00?ie=UTF8&psc=1
 //
+//
+// 2017-01 MDO: re-wrote pot hysterisis code, which suffered from two-person split-personality stream-of-consciousness coding syndrome.
+//    Using AREF, sampling pot 500 times gives a range of [0,6] (example: values 708 through 713). Hysterisis parameter is set at 4 which
+//    will handle this but means that we can only have intervals of 8 since hysterisis < interval size. Thus 128 intervals.
+//
 // connections:
 // 
 // Nano pin 5V      LEDstick VCC
@@ -52,7 +55,7 @@
 // Nano pin GND     Pot GND (other side)
 // Nano pin A-1     Pot middle
 
-// Recommendations
+// Recommendations - of course I ignored these!  ;^)
 //    Before connecting the WS2812 to a power source, connect a big capacitor from power to ground. A cap between 100microF and 1000microF should be good. Try to place this cap as close to your WS2812 as possible.
 //    Electrolytic Decoupling Capacitors 
 //
@@ -68,7 +71,7 @@
 
 // we will use analog pin A1 to read the potentiometer
 //   leaves a little space between that and AREF
-// BE SURE TO CONNECT THE AREF PIN!!!
+// BE SURE TO CONNECT THE AREF PIN!!! MAKES READINGS MUCH LESS NOISY
 // the potentiometer adjusts the desired car position (fine grain)
 #define ANLG_PIN 1
 
@@ -81,40 +84,43 @@
 // we use the range value from the Ultrasonic sensor to decide we are in one of
 //    ULTRA_REGION_NUM display ranges (or OFF)
 //    ranges are mapped to appropriate entry in led_ultra_range_mapping[] array
-#define ULTRA_REGION_NUM 6
+#define ULTRA_REGION_NUM 7 // number of valid ranges; index = 0 means OFF
 
 // These are the definitions for our software hysteresis
-// the setup() routine initializes the interval_* arrays
 // NOTE FIXME DEPENDS ON POT_RAW_NUM_INTVL BEING A POWER OF 2 AND EXACTLY DIVISIBLE INTO COUNTS
-#define POT_RAW_NUM_INTVL 16 // size of an interval
-#define POT_HYST_DELTA 4 // size extra size of hysteresis each side in counts
 #define POT_RAW_NUM_COUNTS 1024 // so max val is 1023
-int interval_low[POT_RAW_NUM_INTVL];
-int interval_high[POT_RAW_NUM_INTVL];
-int interval_hyst_low[POT_RAW_NUM_INTVL];
-int interval_hyst_high[POT_RAW_NUM_INTVL];
+#define POT_RAW_COUNTS_INTVL 8 // analog counts in each interval
+#define POT_RAW_NUM_INTVL (POT_RAW_NUM_COUNTS/POT_RAW_COUNTS_INTVL) // number of intervals
+#define POT_HYST_DELTA 4 // size extra size of hysteresis each side in counts; MUST be < (POT_RAW_NUM_COUNTS/POT_RAW_NUM_INTVL)
+static int pot_intvl_prev = -99; // no hysterisis first time; this interval will match nothing
+#define DEBUG_POT_HYSTERISIS(A) // Serial.print((A)) to debug
+
+// approximate distances
+// 50.8 cm or 20 inches min
+// 66.04 cm or 26 inches van
+// 81.28 cm or 32 inches middle
+// 96.52 cm or 38 inches car
+// 111.76 cm or 44 inches max
+#define POT_DISTANCE_MIN_CM   50.8
+#define POT_DISTANCE_MAX_CM  111.76
+#define POT_DISTANCE_DELTA_CM ((POT_DISTANCE_MAX_CM-POT_DISTANCE_MIN_CM)/POT_RAW_NUM_INTVL)
 
 // Creates an array with the length set by NUM_LEDS above
 // This is the array the library will read to determine how each LED in the strand should be set
 CRGB led_display[NUM_LEDS];
 CRGB led_ultra_range_mapping[8 * NUM_LEDS] = { \
+    CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,  \
     CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Green,  \
     CRGB::Orange,  CRGB::Orange,  CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Orange, \
     CRGB::Orange,  CRGB::Orange,  CRGB::Orange,  CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Green,   CRGB::Orange, \
-    CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Orange,  CRGB::Orange,  CRGB::Orange,  CRGB::Red,  \
-    CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Orange,  CRGB::Orange,  CRGB::Red,  \
-    CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Orange,  CRGB::Red,  \
-    CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,  \
-    CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black,   CRGB::Black};
+    CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Orange,  CRGB::Orange,  CRGB::Orange,  CRGB::Red,    \
+    CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Orange,  CRGB::Orange,  CRGB::Red,    \
+    CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Orange,  CRGB::Red,    \
+    CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red,     CRGB::Red};
 
 int blnk_cnt = 0;
 int blnk_on = 0;
 #define BLNK_CNT_MAX 5
-
-// for now - DEBUG FIXME calc range from potentiometer
-#define RANGE_MVNG_AVG_NUM 8
-int range_mvng_avg_array[RANGE_MVNG_AVG_NUM] = { 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000 };
-int range_mvng_avg_idx = 0;
 
 // Now we're going to initialize an instance from the Ultrasonic Sensor library with our pin numbers
 // The sensor itself needs four leads: ground, vcc, trigger, and echo
@@ -140,10 +146,62 @@ void copy_leds(int idx, CRGB *from, CRGB *to)
   }
 }
 
+int pot_intvl_with_hysterisis(int pot_read) {
+    int pot_intvl = pot_read / POT_RAW_COUNTS_INTVL;
+    DEBUG_POT_HYSTERISIS(pot_intvl_prev); DEBUG_POT_HYSTERISIS(" <-- pot_intvl_prev "); DEBUG_POT_HYSTERISIS(pot_intvl); DEBUG_POT_HYSTERISIS(" <-- pot_intvl_raw ");
+    // now to check for hysterisis
+    if ( (pot_intvl - pot_intvl_prev) == 1) {
+        DEBUG_POT_HYSTERISIS((pot_read - POT_HYST_DELTA) / POT_RAW_COUNTS_INTVL ); DEBUG_POT_HYSTERISIS(" <-- hyst check due to +1 ");
+        if ( ( (pot_read - POT_HYST_DELTA) / POT_RAW_COUNTS_INTVL )  == pot_intvl_prev) {
+            pot_intvl = pot_intvl_prev;
+            DEBUG_POT_HYSTERISIS(pot_intvl); DEBUG_POT_HYSTERISIS(" <-- pot_intvl_adjust due to +1 ");
+        }
+    } else if ( (pot_read - POT_HYST_DELTA) == -1) {
+        DEBUG_POT_HYSTERISIS((pot_read - POT_HYST_DELTA) / POT_RAW_COUNTS_INTVL ); DEBUG_POT_HYSTERISIS(" <-- hyst check due to -1 ");
+        if ( ( (pot_read + POT_HYST_DELTA) / POT_RAW_COUNTS_INTVL )  == pot_intvl_prev) {
+            pot_intvl = pot_intvl_prev;
+            DEBUG_POT_HYSTERISIS(pot_intvl); DEBUG_POT_HYSTERISIS(" <-- pot_intvl_adjust due to -1 ");
+        }
+    }   // end if might need hysterisis adjustment
+    pot_intvl_prev = pot_intvl;
+    return(pot_intvl);
+}   // end Pot_intvl_with_hysterisis()
+
+// with AREF and hysterisis don't need moving average
+int pot_desired_distance() {
+    int pot_read = analogRead(ANLG_PIN); // This checks the pot connected to Analog Pin - and gives us a value between 0-1024
+    DEBUG_POT_HYSTERISIS(pot_read); DEBUG_POT_HYSTERISIS(" <-- analog ");
+    // get potentiometer interval number [0,POT_RAW_NUM_INTVL)
+    int pot_intvl = pot_intvl_with_hysterisis(pot_read);
+    DEBUG_POT_HYSTERISIS(pot_intvl); DEBUG_POT_HYSTERISIS(" <-- intvl ");
+    // convert to distance in cm (psuedo-rounded)
+    int pot_dist = POT_DISTANCE_MIN_CM +0.5 + pot_intvl * POT_DISTANCE_DELTA_CM;
+    Serial.print(pot_dist);
+    Serial.print(" <-- dist");
+    Serial.print("\n"); // don't use println so can use DEBUG_POT_HYSTERISIS()
+    return(pot_dist);
+}   // end pot_desired_distance()
+
+// pot_dist is the desired distance
+// ultra_distance is the actual distance
+// approximate distances
+// 50.8 cm or 20 inches min
+// 66.04 cm or 26 inches van
+// 81.28 cm or 32 inches middle
+// 96.52 cm or 38 inches car
+// 111.76 cm or 44 inches max
+
+int calc_led_idx(int pot_dist, int ultra_dist) {
+   int idx = 0;
+   return(idx);  
+}
+#define POT_DISTANCE_MIN_CM   50.8
+#define POT_DISTANCE_MAX_CM  111.76
+#define POT_DISTANCE_DELTA_CM ((POT_DISTANCE_MAX_CM-POT_DISTANCE_MIN_CM)/POT_RAW_NUM_INTVL)
+
 // setup()
 //   initializes FastLED library for our config
 //   initializes serial port
-//   initializes hysteresis range arrays
 void setup()
 {
   FastLED.addLeds<NEOPIXEL,LED_DATA_PIN>(led_display, NUM_LEDS);
@@ -151,37 +209,17 @@ void setup()
 
   Serial.println("setup");
   
-  int raw_num_per_intvl = (POT_RAW_NUM_COUNTS / POT_RAW_NUM_INTVL);
-  for (int i = 0; i < POT_RAW_NUM_INTVL; i += 1)
-  {
-    interval_low[i] = raw_num_per_intvl * i;
-    interval_high[i] = interval_low[i] + raw_num_per_intvl - 1;
-    interval_hyst_low[i] = max(0, interval_low[i] - POT_HYST_DELTA);
-    interval_hyst_high[i] = min(POT_RAW_NUM_COUNTS - 1, interval_high[i] + POT_HYST_DELTA);
-    Serial.print(i);
-    Serial.print(", ");
-    Serial.print(interval_low[i]);
-    Serial.print(", ");
-    Serial.print(interval_high[i]);
-    Serial.print(", ");
-    Serial.print(interval_hyst_low[i]);
-    Serial.print(", ");
-    Serial.println(interval_hyst_high[i]);
-  }
-  delay(1000);
+  delay(100);
 }
 
 // loop()
 //    sorry; gets a little complex here ... ;^)
 //
-//    we read the potentiometer and calculate its interval using hysteresis
+//    we read the potentiometer and calculate its distance setting using hysteresis
 //       note: normally the potentiometer only moved during calibration
-//    using this we calculate the range_regions[] to use for the ultrasonic ranging
-//
-//    we get the range from the instance of the Ultrasonic library and pass it
-//       through a moving average (a low pass filter (LPF) with some zeros)
-//
-//    we calculate what region the LPF range is in with a check for "no ultrasound bounce" return
+//    we compare this desired distance with that from LPF
+//    If  "no ultrasound bounce" return, special case index into led_ultra_range_mapping[]
+//    we calculate what region the LPF range is in
 //    the index of that region is the index into led_ultra_range_mapping[] for display
 //       so we call copy_leds() with that index
 //    copy_leds() takes care of LED blinking using a count based on time
@@ -190,8 +228,8 @@ void loop() {
   
   static int idx = 0;
   static int range_regions_min[ULTRA_REGION_NUM] = { 42, 32, 27, 17, 12, 7 };
-  static int range_regions[ULTRA_REGION_NUM] = { 42, 32, 27, 17, 12, 7 };
-  static int range_avg;
+  static int range_regions    [ULTRA_REGION_NUM] = { 42, 32, 27, 17, 12, 7 };
+  static int range;
   static int potIntvl = 0; // number calculated from potentiometer using hysteresis; previous at entry
   static int once = 0;
 
@@ -201,61 +239,16 @@ void loop() {
     Serial.println("loop");
   }
 
-  int aRead = analogRead(ANLG_PIN); // This checks the pot connected to Analog Pin - and gives us a value between 0-1024
-
-  // calculate next potIntvl using hysteresis. If still in expanded region of past poTintvl, no change
-  if ((aRead <= interval_hyst_low[potIntvl]) || (aRead >= interval_hyst_high[potIntvl]))
-  {
-    for (int i = 0; i < POT_RAW_NUM_INTVL; i += 1)
-    {
-      if ((aRead >= interval_low[i]) && (aRead <= interval_high[i]))
-      {
-        potIntvl = i;
-        break;
-      }
-    }
-  }
-  for (int i = 0; i < ULTRA_REGION_NUM; i += 1)
-  {
-    range_regions[i] = range_regions_min[i] + potIntvl;
-  }
+  // this gives us a number of centimeters we want to call good
+  int pot_dist = pot_desired_distance();
   
-  //Serial.print(aRead);
-  //Serial.print(", ");
-  //Serial.println(potVal);
-
-  // This creates an integer variable called "range", then fills it with the range reading from the Ultrasonic sensor in centimeters
-  int range=(ultrasonic.Ranging(CM));
-  //Serial.println(range);
-
-  // this maintains the moving average data
-  range_mvng_avg_array[range_mvng_avg_idx] = range;
-  range_mvng_avg_idx = (range_mvng_avg_idx + 1) % RANGE_MVNG_AVG_NUM;
-  // brute force moving average
-  range_avg = 0;
-  for (int i = 0; i < RANGE_MVNG_AVG_NUM; i += 1)
-  {
-    //Serial.print(range_avg);
-    //Serial.print(", ");
-    range_avg += range_mvng_avg_array[i];
-  }
-  //Serial.println(range_avg);
-  range_avg /= RANGE_MVNG_AVG_NUM;
-  //Serial.println(range_avg);
-
-  // compute regions for range measurement
+  // the range reading from the Ultrasonic sensor in centimeters
+  int ultra_dist=(ultrasonic.Ranging(CM));
+  idx = calc_led_idx(pot_dist, ultra_dist);
+  //Serial.println(idx);
+  // we want [0,ULTRA_REGION_NUM] currently [0-7] (note: enpoint inclusive
   // see what region idx we are in - farthest away is smallest idx; idx=7 is LED OFF
-  idx = ULTRA_REGION_NUM;
-  for (int i = 0; i < ULTRA_REGION_NUM; i += 1)
-  {
-    if (range_avg > range_regions[i])
-    {
-      idx = i;
-      break;
-    }
-  }
-  if (range_avg > 2200) idx = 7; // DEBUG FIXME what is correct bounce not returning value
-  Serial.println(idx);
+
 
   copy_leds(idx, led_ultra_range_mapping, led_display);
   FastLED.setBrightness(60);
